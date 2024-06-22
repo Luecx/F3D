aa9836211853cda291b346f4297d86a3b78fad8f
Finn Eggers <finn@Finns-MacBook-Pro.local>
2024-06-22 22:02:48 +0200

    update ecs

--- include/ecs.h
+++ include/ecs.h
@@ -9,7 +9,14 @@
 #ifndef ECS_ECS_EVENT_H_
 #define ECS_ECS_EVENT_H_
 
-#include <memory>
+// begin --- hash.h --- 
+
+//
+// Created by Luecx on 15.06.2021.
+//
+
+#ifndef ECS_ECS_HASH_H_
+#define ECS_ECS_HASH_H_
 
 // begin --- types.h --- 
 
@@ -53,6 +60,32 @@ typedef std::type_index Hash;
 
 
 // end --- types.h --- 
+
+
+
+namespace ecs {
+
+/**
+ * @brief Generates a hash for the specified type.
+ *
+ * This function template generates a hash value for a given type using
+ * the C++ type_index and typeid mechanisms. This is useful for type identification
+ * and comparison within the ECS (Entity Component System) framework.
+ *
+ * @tparam T The type for which the hash is to be generated.
+ * @return Hash The generated hash value representing the type.
+ */
+template<typename T>
+Hash get_type_hash() {
+    return std::type_index(typeid(T));
+}
+
+}    // namespace ecs_
+
+#endif    // ECS_ECS_HASH_H_
+
+
+// end --- hash.h --- 
 
 
 
@@ -106,6 +139,8 @@ struct EventListenerID {
 
 
 
+#include <memory>
+
 namespace ecs {
 
 struct EventListenerBase {
@@ -154,41 +189,6 @@ struct EventListener : public EventListenerBase {
 
 #ifndef ECS_ECS_COMPONENT_H_
 #define ECS_ECS_COMPONENT_H_
-
-// begin --- hash.h --- 
-
-//
-// Created by Luecx on 15.06.2021.
-//
-
-#ifndef ECS_ECS_HASH_H_
-#define ECS_ECS_HASH_H_
-
-namespace ecs {
-
-/**
- * @brief Generates a hash for the specified type.
- *
- * This function template generates a hash value for a given type using
- * the C++ type_index and typeid mechanisms. This is useful for type identification
- * and comparison within the ECS (Entity Component System) framework.
- *
- * @tparam T The type for which the hash is to be generated.
- * @return Hash The generated hash value representing the type.
- */
-template<typename T>
-Hash get_type_hash() {
-    return std::type_index(typeid(T));
-}
-
-}    // namespace ecs_
-
-#endif    // ECS_ECS_HASH_H_
-
-
-// end --- hash.h --- 
-
-
 
 // begin --- ecs_base.h --- 
 
